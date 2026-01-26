86943ea2a2906273c09753b1026082383fb66051
Nancyweng5 <wengalex55@gmail.com>
2026-01-26 14:10:05 -0500

    .

--- wish/src/main.js
+++ wish/src/main.js
@@ -1,24 +1,40 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const standard5stars = [
+  { name: "Diluc", rarity: 5 },
+  { name: "Jean", rarity: 5 },
+  { name: "Mona", rarity: 5 },
+  { name: "Qiqi", rarity: 5 },
+  { name: "Keqing", rarity: 5 },
+  { name: "Tighnari", rarity: 5 },
+  { name: "Dehya", rarity: 5 },
+   { name: "Yumemizuki Mizuki", rarity: 5 },
+];
+const limited5stars = [
+  { name: "Venti", rarity: 5 },
+  { name: "Zhongli", rarity: 5 },
+  { name: "Raiden Shogun", rarity: 5 },
+  { name: "Nahida", rarity: 5 },
+  { name: "Hu Tao", rarity: 5 },
+  { name: "Ganyu", rarity: 5 },
+  { name: "Neuvillette", rarity: 5 }
+]
+const fourStarWeapons = [
+  { name: "Favonius Sword", rarity: 4 },
+  { name: "Sacrificial Sword", rarity: 4 },
+  { name: "The Stringless", rarity: 4 },
+  { name: "The Widsith", rarity: 4 },
+  { name: "Rust", rarity: 4 },
+  { name: "Dragon's Bane", rarity: 4 },
+  { name: "Favonius Lance", rarity: 4 },
+  { name: "Sacrificial Fragments", rarity: 4 }
+];
+const threeStarWeapons = [
+  { name: "Cool Steel", rarity: 3 },
+  { name: "Harbinger of Dawn", rarity: 3 },
+  { name: "Thrilling Tales of Dragon Slayers", rarity: 3 },
+  { name: "Debate Club", rarity: 3 },
+  { name: "Slingshot", rarity: 3 },
+  { name: "Sharpshooter's Oath", rarity: 3 }
+];
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+let pity = 0;
+let pullsSinceFour = 0;
